e2a9c8f670ec08608ef9e1dc69b3e38b890bcbf2
MattMcHatton <Matt.McHatton@dialexa.com>
2022-04-12 13:21:51 -0500

    updating create table sql

--- ping-pong-tracker/public/create-tables.sql
+++ ping-pong-tracker/public/create-tables.sql
@@ -2,8 +2,10 @@ create table players (
 	guid UNIQUEIDENTIFIER NOT NULL PRIMARY KEY default NEWID(),
 	username varchar(50) NOT NULL ,
 	password varchar(25),
-	created_at varchar NOT NULL default GETDATE(),
-	updated_at varchar NOT NULL default GETDATE(),
+	f_name varchar(25) NOT NULL,
+	l_name varchar(30) NOT NULL,
+	created_at datetime NOT NULL default GETDATE(),
+	updated_at datetime NOT NULL default GETDATE(),
 	active bit NOT NULL default 0
 )
 
@@ -13,14 +15,14 @@ create table rankings (
 	losses int,
 	player_rank int,
 	previous_rank int,
-	updated_at varchar
+	updated_at datetime default GETDATE()
 )
 
 create table matches (
 	match_guid UNIQUEIDENTIFIER NOT NULL PRIMARY KEY default NEWID(),
 	home_user_id UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES players(guid),
 	away_user_id UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES players(guid),
-	match_date varchar NOT NULL ,
+	match_date datetime NOT NULL ,
 	winner UNIQUEIDENTIFIER  NOT NULL FOREIGN KEY REFERENCES players(guid)
 )
 
