5a42bc5f365091814793e5ec02f791c3ce612750
MattMcHatton <Matt.McHatton@dialexa.com>
2022-04-27 09:40:59 -0500

    adding dotenv and committing config file

---
+++ ping-pong-tracker/src/config/config.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+
+export var config = {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+}
