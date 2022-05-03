fae50077dabb6e43ddc19de5eb68a56c62a5af64
MattMcHatton <Matt.McHatton@dialexa.com>
2022-05-03 13:57:28 -0500

    adding functions for GET matches

--- ping-pong-tracker/src/models/match_model.ts
+++ ping-pong-tracker/src/models/match_model.ts
@@ -1,3 +1,4 @@
+import { userInfo } from 'os'
 import conn from '../util/database.js'
 
 export class Match {
@@ -8,107 +9,51 @@ export class Match {
         let { home_user,away_user,match_date,user } = queryParams
 
         try {
-            let records
-            //If single user and date, send all records for that user and date, otherwise send all records for that user
-            if(user) {
-                let user_guid = await this._getUserGuid(user)
-                if (match_date) {
-                    records = await conn.select().table('matches').where({
-                        home_user_id: user_guid,
-                        match_date: match_date
-                    }).orWhere({
-                        away_user_id: user_guid,
-                        match_date: match_date
-                    })
-    
-                    return {
-                        status: 200,
-                        body: records
-                    }
-                }
 
-                records = await conn.select().table('matches').where({
-                    home_user_id: user_guid,
-                }).orWhere({
-                    away_user_id: user_guid,
-                })
-
+            //Edge case when home user, away user, and single user are passed
+            if (home_user && away_user && user) {
                 return {
-                    status: 200,
-                    body: records
+                    status: 409,
+                    body: "Cannot pass multi user and single user params."
                 }
             }
 
-            //If match date is present, check for users. If both users are not present, just pass all matches for that date
+            //If single user and date, send all records for that user and date, otherwise send all records for that user
+            if(user && match_date) {
+                return await this._singleUserAndDateGet(user, match_date)
+            }
+
+            if(user) {
+                return await this._singleUserNoDateGet(user)
+            }
+
+            //If match date and both users are present
+            if(home_user && away_user && match_date) {
+                return await this._multiUserAndDateGet([home_user, away_user], match_date)
+            }
+
+            //If only match date is present
             if (match_date) {
-                if(home_user && away_user) {
-                    let home_user_guid = await this._getUserGuid(home_user)
-                    let away_user_guid = await this._getUserGuid(away_user)
-
-                    records = await conn.select().table('matches').where({
-                        home_user_id: home_user_guid,
-                        away_user_id: away_user_guid,
-                        match_date: match_date
-                    }).orWhere({
-                        home_user_id: away_user_guid,
-                        away_user_id: home_user_guid,
-                        match_date: match_date
-                    })
-    
-                    return {
-                        status: 200,
-                        body: records
-                    }
-                }
-
-                records = await conn.select().table('matches').where({
-                    match_date: match_date
-                })
-                
-                return {
-                    status: 200,
-                    body: records
-                }
-
+                return await this._dateOnlyGet(match_date)
             } 
 
-            //If both users present, pass all of their matches
+            //If both users present, but no match date pass all of their matches
             if (home_user && away_user) {
-
-                let home_user_guid = await this._getUserGuid(home_user)
-                let away_user_guid = await this._getUserGuid(away_user)
-    
-                records = await conn.select().table('matches').where({
-                    home_user_id: home_user_guid,
-                    away_user_id: away_user_guid,
-                }).orWhere({
-                    home_user_id: away_user_guid,
-                    away_user_id: home_user_guid,
-                })
-    
-                return {
-                    status: 200,
-                    body: records
-                }
-                
+                return await this._multiUserNoDateGet([home_user, away_user])
             }
 
             //If no params, send all records
-            records = await conn.select().table('matches') 
+            let records = await conn.select().table('matches') 
             return {
                 status: 200,
                 body: records
             }
-
-
-
         } catch (err) {
             return {
                 status: 500,
                 body: err
             }
         }
-
     }
 
     static async recordMatch(body: object){ 
@@ -271,6 +216,100 @@ export class Match {
 
     static _emptyParams(params: object){
         return (Object.keys(params).length === 0) === true ? true : false
+    }
+
+    static async _singleUserAndDateGet(user: String, match_date: any){
+        let records
+        let user_guid = await this._getUserGuid(user)
+
+        records = await conn.select().table('matches')
+        .whereRaw('(CONVERT(char(10), match_date ,126)) = ?', [match_date])
+        .andWhere({
+            home_user_id: user_guid,
+        })
+        .orWhereRaw('(CONVERT(char(10), match_date ,126)) = ?', [match_date])
+        .andWhere({
+            away_user_id: user_guid,
+        })
+
+        return {
+            status: 200,
+            body: records
+        }
+    }
+
+    static async _singleUserNoDateGet(user: String) {
+
+        let user_guid = await this._getUserGuid(user)
+
+        let records = await conn.select().table('matches').where({
+            home_user_id: user_guid,
+        }).orWhere({
+            away_user_id: user_guid,
+        })
+
+        return {
+            status: 200,
+            body: records
+        }
+    }
+
+    static async _multiUserAndDateGet(users: String[], match_date: any){
+
+        let records
+        let [home_user, away_user] = users
+
+        let home_user_guid = await this._getUserGuid(home_user)
+        let away_user_guid = await this._getUserGuid(away_user)
+
+        records = await conn.select().table('matches')
+        .whereRaw('(CONVERT(char(10), match_date ,126)) = ?', [match_date])
+        .andWhere({
+            home_user_id: home_user_guid,
+            away_user_id: away_user_guid,
+        }).orWhereRaw('(CONVERT(char(10), match_date ,126)) = ?', [match_date])
+        .andWhere({
+            home_user_id: away_user_guid,
+            away_user_id: home_user_guid,
+        })
+
+        return {
+            status: 200,
+            body: records
+        } 
+    }
+
+    static async _multiUserNoDateGet(users: String[]){
+
+        let records
+        let [home_user, away_user] = users
+
+        let home_user_guid = await this._getUserGuid(home_user)
+        let away_user_guid = await this._getUserGuid(away_user)
+
+        records = await conn.select().table('matches')
+        .where({
+            home_user_id: home_user_guid,
+            away_user_id: away_user_guid,
+        }).orWhere({
+            home_user_id: away_user_guid,
+            away_user_id: home_user_guid,
+        })
+
+        return {
+            status: 200,
+            body: records
+        } 
+    }
+
+    static async _dateOnlyGet(match_date: any){
+        let records = await conn.select().table('matches')
+            .whereRaw('(CONVERT(char(10), match_date ,126)) = ?', [match_date])
+
+        return {
+            status: 200,
+            body: records
+        }
     }
 
 }
