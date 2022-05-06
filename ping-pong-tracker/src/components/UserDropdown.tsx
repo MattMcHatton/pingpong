4e2464bf4164658deb7d9948a9ebadcea89c1ee9
MattMcHatton <Matt.McHatton@dialexa.com>
2022-05-06 17:12:56 -0500

    adding UserDropdown

--- ping-pong-tracker/src/components/UserDropdown.tsx
+++ ping-pong-tracker/src/components/UserDropdown.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react'
-import { Select, MenuItem, InputLabel, FormControl, SelectChangeEvent } from '@mui/material';
-import { useState } from 'react';
+import { Select, MenuItem, InputLabel, FormControl, SelectChangeEvent } from '@mui/material'
+import { useState } from 'react'
+import useAsyncEffect from 'use-async-effect'
 
 export default function UserDropdown(props) {
     
+    const [users, set_users] = useState([])
     const [home_user, set_home_user] = useState("")
     const [away_user, set_away_user] = useState("")
+    
+    useAsyncEffect(async () => await getUsers(), [])
+
+    async function getUsers() {
+        let users = await fetch('/user')
+        let users_json = await users.json()
+        return set_users(users_json)
+    }
 
     if(props.type === 'away'){
         return (
@@ -17,9 +27,14 @@ export default function UserDropdown(props) {
                     value={ home_user }
                     onChange={(e) => set_home_user(e.target.value)}
                     >
-                        <MenuItem value={'matt.mchatton@dialexa.com'}>Matt McHatton</MenuItem>
+                        <MenuItem>
+                            { users.map( (x,y) => 
+                                <option key={y}>{x}</option> )
+                            }
+                        </MenuItem> 
+                        {/* <MenuItem value={'matt.mchatton@dialexa.com'}>Matt McHatton</MenuItem>
                         <MenuItem value={'matt.tucker@dialexa.com'}>Matt Tucker</MenuItem>
-                        <MenuItem value={'brandon.harper@dialexa.com'}>Brandon Harper</MenuItem>
+                        <MenuItem value={'brandon.harper@dialexa.com'}>Brandon Harper</MenuItem> */}
                     </Select>
             </FormControl>
         )
